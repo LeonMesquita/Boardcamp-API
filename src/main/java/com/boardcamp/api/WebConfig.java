4362652ff61a09efdb0aca3a66cca53613d08b42
LeonMesquita <leonmesquita16@gmail.com>
2025-05-04 17:15:09 -0300

    First commit

---
+++ src/main/java/com/boardcamp/api/WebConfig.java
@@ -0,0 +1,17 @@
+package com.boardcamp.api;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // aplica para todos os endpoints
+                .allowedOrigins("*") // permite qualquer origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
